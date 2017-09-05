8da4f4e8151f3d8339a0f7e79f1cb054aad9971c
Tony Liu <388848@163.com>
2017-09-05 09:25:23 +0800

    Implement user's info

--- shvad-checkin.py
+++ shvad-checkin.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flaskext.mysql import MySQL
+from json import dumps
+from pymysql.cursors import DictCursor
 
 app = Flask(__name__)
 app.config["MYSQL_DATABASE_HOST"] = "127.0.0.1"
@@ -13,6 +15,29 @@ mysql.init_app(app)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/info', methods=["POST"])
+def info():
+    stu_name = request.form.get("stu_name")
+    if stu_name:
+        result = {}
+        cur = DictCursor(mysql.get_db())
+        cur.execute("SELECT * FROM attend WHERE stu_name=%s;", stu_name)
+        data = cur.fetchone()
+        if data:
+            result["id"] = data.pop("id")
+            result["attendance"] = data.pop("attendance")
+            for item in data:
+                if isinstance(data[item], str):
+                    data[item] = data[item].strip().replace("\n", "<br/>")
+            result["data"] = data
+            result["ret"] = 1
+        else:
+            result["ret"] = -1
+        return dumps(result, ensure_ascii=False)
+    else:
+        return '{"ret": -1}'
 
 
 if __name__ == '__main__':
